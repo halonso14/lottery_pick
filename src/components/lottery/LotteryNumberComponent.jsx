61b99517be3318fdf7f9d8881bc8fb4b16e5a46d
Roman Seo <hdsbsb@naver.com>
2021-07-30 17:58:03 +0900

    feat: ADD style to LotteryNumberComponent

--- src/components/lottery/LotteryNumberComponent.jsx
+++ src/components/lottery/LotteryNumberComponent.jsx
@@ -1,17 +1,49 @@
 import PropTypes from 'prop-types';
 import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+// const LotteryNumberBox = styled.div.attrs((props) => ({
+//   className: props.className,
+// }))`
+
+const LotteryNumberBox = styled.div`
+  display: inline-block;
+  border-radius: 3px;
+  margin: 10px;
+  width: 80px;
+  justify-content: center;
+  flex-direction: column;
+  height: 80px;
+  border: solid 2px
+    ${(props) => {
+      switch (props.className) {
+        case 'selected':
+          return 'green';
+        case 'blocked':
+          return 'red';
+        case 'none':
+        default:
+          return 'black';
+      }
+    }};
+`;
 
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 function LotteryNumberComponent(props) {
   const { state, number, toggleNumber } = props;
+
   const handleToggleNumber = useCallback(
     () => toggleNumber(state),
     [toggleNumber],
   );
   return (
-    <div className={state} onClick={handleToggleNumber} onKeyPress={undefined}>
+    <LotteryNumberBox
+      className={state}
+      onClick={handleToggleNumber}
+      onKeyPress={undefined}
+    >
       {number}
-    </div>
+    </LotteryNumberBox>
   );
 }
 
